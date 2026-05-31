bc1dea737301ef5d02b7a75dc5ee0f9a1820cb89
Gustavo Falavigna <gfalav@hotmail.com>
2026-05-31 12:07:32 -0300

    push notifications routes ready

--- lib/shared/controllers/notifications_controller.dart
+++ lib/shared/controllers/notifications_controller.dart
@@ -19,22 +19,15 @@ class NotificationController extends GetxController {
       sound: true,
     );
 
-    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
-      print('Permiso concedido por el usuario');
-    }
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {}
 
     // 2. Obtener el token del dispositivo (opcional, para enviarlo a tu backend)
     String? token = await _fcm.getToken();
     this.token.value = token ?? '';
-    print("FCM Token: $token");
 
     // 3. Manejar mensajes en PRIMER PLANO (Foreground)
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print('----------------------------------------------------------------');
-      print('----------------------------------------------------------------');
-      print('Mensaje recibido en primer plano: ${message.notification?.title}');
-
-      // Aquí puedes usar el snackbar de GetX para mostrar una notificación interna
+      Get.toNamed('/notifications');
       Get.snackbar(
         message.notification?.title ?? 'Nueva notificación',
         message.notification?.body ?? '',
@@ -45,29 +38,18 @@ class NotificationController extends GetxController {
 
     // 4. Manejar clics cuando la app está en SEGUNDO PLANO (Background)
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      print('----------------------------------------------------------------');
-      print('----------------------------------------------------------------');
-      print('El usuario tocó la notificación en segundo plano');
       _handleNotificationClick(message);
     });
 
     // 5. Manejar clics si la app estaba COMPLETAMENTE CERRADA (Terminated)
     RemoteMessage? initialMessage = await _fcm.getInitialMessage();
     if (initialMessage != null) {
-      print('----------------------------------------------------------------');
-      print('----------------------------------------------------------------');
-      print('La app se abrió desde una notificación cerrada');
       _handleNotificationClick(initialMessage);
     }
   }
 
   // Función para navegar usando GetX según los datos de la notificación
   void _handleNotificationClick(RemoteMessage message) {
-    // Puedes enviar datos personalizados en el "payload" (data) de Firebase
-    if (message.data['screen'] == 'chat') {
-      Get.toNamed('/chat', arguments: message.data['chatId']);
-    } else {
-      Get.toNamed('/notifications');
-    }
+    Get.toNamed('/notifications');
   }
 }
